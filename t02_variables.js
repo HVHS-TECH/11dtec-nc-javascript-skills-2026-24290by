9cbef9a15dc2ac3f33cd4cd90d68f3018a516520
24290by <24290by@hvhs.school.nz>
2026-04-29 02:31:46 +0000

    Apr 29, 2026, 2:31 AM

---
+++ t02_variables.js
@@ -0,0 +1,6 @@
+let username = "Ben";
+let year = 2026;
+let age = 28;
+let money = 20;
+
+console.log("hi "+ username + " as of " + year + " you are " + age + " years old. You have " + money + "dollars");
