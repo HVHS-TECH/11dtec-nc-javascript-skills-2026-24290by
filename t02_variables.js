e5f3ff28cb317fb557a4191648bdcc9665b84530
24290by <24290by@hvhs.school.nz>
2026-04-29 02:49:15 +0000

    Apr 29, 2026, 2:49 AM

--- t02_variables.js
+++ t02_variables.js
@@ -3,4 +3,4 @@ let year = 2026;
 let age = 28;
 let money = 20;
 
-console.log("hi "+ username + " as of " + year + " you are " + age + " years old. You have " + money + "dollars");
+console.log("hi "+ username + " as of " + year + " you are " + age + " years old. You have " + money + "dollars")
--- t03_maths.js
+++ t03_maths.js
@@ -1,1 +1,17 @@
-/* Paste your code from task 2 here */
+
+Name of task: Maths 
+
+
+//Variables 
+let username = "Ben";
+let year = 2026;
+let age = 28;
+let money = 20;
+let birthYear;
+let oldAge;
+let oldAge = age + 10;
+let birthYear - year - age;
+
+console.log("hi "+ username + " as of " + year + " you are " + age + " years old. You have " + money + "dollars")
+console.log( "You were born in " + birthyear)
+
