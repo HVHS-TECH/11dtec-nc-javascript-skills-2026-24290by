a8fd93b90b0467f6374b6956ea052fa3dbc8c128
24290by <24290by@hvhs.school.nz>
2026-04-29 03:00:26 +0000

    Apr 29, 2026, 3:00 AM

--- t03_maths.js
+++ t03_maths.js
@@ -24,7 +24,7 @@ console.log( "You were born in " + birthYear)
 
 console.log("hi "+ username + " as pf " + year + "you are " + age + "years old. " + age + " years old ")
 console.log("you were born in " + birthYear)
-console.log("In 10 years you will be " + oldAge "years old ")
+console.log("In 10 years you will be " + oldAge + " years old")
 console.log("you havde " + money + "dollars ")
 console.log("you spend half your money, now you have" + )
 console.log("Then you get $3, now you have ")
