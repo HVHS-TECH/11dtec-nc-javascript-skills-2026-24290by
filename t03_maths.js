6bf8742eb2d4d7b54105f59674e5907ca06e9173
24290by <24290by@hvhs.school.nz>
2026-04-29 02:59:02 +0000

    Apr 29, 2026, 2:59 AM

--- t03_maths.js
+++ t03_maths.js
@@ -17,3 +17,18 @@ let oldAge;
 console.log("hi "+ username + " as of " + year + " you are " + age + " years old. You have " + money + "dollars")
 console.log( "You were born in " + birthYear)
 
+
+/******************************
+ Main code
+ ******************************/
+
+console.log("hi "+ username + " as pf " + year + "you are " + age + "years old. " + age + " years old ")
+console.log("you were born in " + birthYear)
+console.log("In 10 years you will be " + oldAge "years old ")
+console.log("you havde " + money + "dollars ")
+console.log("you spend half your money, now you have" + )
+console.log("Then you get $3, now you have ")
+
+/******************************
+ Functions
+ ******************************/
