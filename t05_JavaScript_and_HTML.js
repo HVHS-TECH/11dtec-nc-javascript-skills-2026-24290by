f410dd084ced572bb36c6cdfa8596e4a05280e83
24290by <24290by@hvhs.school.nz>
2026-05-03 22:45:00 +0000

    May 3, 2026, 10:45 PM

---
+++ t05_JavaScript_and_HTML.js
@@ -0,0 +1,26 @@
+/******************************
+ Name of task: Maths
+ ******************************/
+
+
+//Variables 
+let username = "Ben";
+let year = 2026;
+let age = 28;
+let money = 20;
+let birthYear;
+let oldAge;
+ oldAge = age + 10;
+ birthYear - year - age;
+
+
+/******************************
+ Main code
+ ******************************/
+
+OUTPUT.innerHTML += "
+OUTPUT.innerHTML += "
+OUTPUT.innerHTML += "
+OUTPUT.innerHTML += "
+OUTPUT.innerHTML += "
+OUTPUT.innerHTML += "
