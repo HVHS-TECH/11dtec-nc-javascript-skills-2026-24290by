83d72651ea161cc89dae1c6d3475c9044576f04d
24290by <24290by@hvhs.school.nz>
2026-05-04 23:58:15 +0000

    May 4, 2026, 11:58 PM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -13,11 +13,11 @@ let oldAge;
  oldAge = age + 10;
  birthYear - year - age;
 
-
+let OUTPUT = document.getElementById("spaceForJavaScriptOutput")
 /******************************
  Main code
  ******************************/
-let OUTPUT = document.getElementById("spaceForJavaScriptOutput")
+
 OUTPUT.innerHTML += ("<p>hi " + Username + " as of " + year + " you are " + age + " years old.</p>");
 OUTPUT.innerHTML += ("<p>you were born in " + birthYear + "</p>");
 OUTPUT.innerHTML += ("<p>in 10 years you will be " + oldAge + "</p>");
