26e7a07ffd3b9e8cfa41f94682181f7b3fe93680
24290by <24290by@hvhs.school.nz>
2026-05-05 00:00:45 +0000

    May 5, 2026, 12:00 AM

--- t06_functions.js
+++ t06_functions.js
@@ -1,1 +1,25 @@
-/* Paste your code from task 5 here */
+/******************************
+ Name of task: Maths
+ ******************************/
+
+
+//Variables 
+let username = "Ben";
+let year = 2026;
+let age = 28;
+let money = 20;
+let birthYear;
+let oldAge;
+ oldAge = age + 10;
+ birthYear - year - age;
+
+let OUTPUT = document.getElementById("spaceForJavaScriptOutput")
+/******************************
+ Main code
+ ******************************/
+
+OUTPUT.innerHTML += ("<p>hi " + " as of " + year + " you are " + age + " years old.</p>");
+OUTPUT.innerHTML += ("<p>you were born in " + birthYear + "</p>");
+OUTPUT.innerHTML += ("<p>in 10 years you will be " + oldAge + "</p>");
+OUTPUT.innerHTML += ("<p>you have " + halfMoney + " pounds</p>");
+OUTPUT.innerHTML += ("<p>you have " + money + " pounds</p>");
