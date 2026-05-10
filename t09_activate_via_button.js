cf9dd2fe0a81f5ebde831b6a255d8793653c156d
24290by <24290by@hvhs.school.nz>
2026-05-10 22:34:04 +0000

    May 10, 2026, 10:34 PM

--- t09_activate_via_button.js
+++ t09_activate_via_button.js
@@ -1,14 +1,29 @@
- function writeLine() {
-    // Add a line to the html page
-    OUTPUT.innerHTML += "<p>Less code matters</p>";
-}
-function displayWelcome(_name, _age){
-  OUTPUT.innerHTML += "<p>Welcome to the page "+_name+"</p>";
-  OUTPUT.innerHTML += "<p>You are "+_age+" years old</p>";
-}
-displayWelcome("Ms Alice", 16);
-  displayWelcome("Mr Bob", 99);
-  
+/******************************
+ Name of task: Maths
+ ******************************/
+
+
+//Variables 
+let username = "Ben";
+let year = 2026;
+let age = 28;
+let money = 20;
+let birthYear;
+let oldAge;
+ oldAge = age + 10;
+ birthYear - year - age;
+
+let OUTPUT = document.getElementById("spaceForJavaScriptOutput")
+/******************************
+ Main code
+ ******************************/
+
+OUTPUT.innerHTML += ("<p>hi " + " as of " + year + " you are " + age + " years old.</p>");
+OUTPUT.innerHTML += ("<p>you were born in " + birthYear + "</p>");
+OUTPUT.innerHTML += ("<p>in 10 years you will be " + oldAge + "</p>");
+OUTPUT.innerHTML += ("<p>you have " + halfMoney + " pounds</p>");
+OUTPUT.innerHTML += ("<p>you have " + money + " pounds</p>");
+
 <button onClick="start()">START</button>
 
   <p>Open the console <code>(Ctrl-shift-i)</code></p>
