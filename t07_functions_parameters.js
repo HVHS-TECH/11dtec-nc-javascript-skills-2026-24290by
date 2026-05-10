5455e11621bd5548e3a6ce684ffe3b0a02df6074
24290by <24290by@hvhs.school.nz>
2026-05-10 22:21:56 +0000

    May 10, 2026, 10:21 PM

--- t07_functions_parameters.js
+++ t07_functions_parameters.js
@@ -1,1 +1,41 @@
-/* Paste your code from task 6 here */
+/******************************
+ Name of task: Function Parameters
+ ******************************/
+
+
+//Variables 
+let username = "Ben";
+let year = 2026;
+let age = 28;
+let money = 20;
+let birthYear;
+let oldAge;
+ oldAge = age + 10;
+ birthYear - year - age;
+
+let OUTPUT = document.getElementById("spaceForJavaScriptOutput")
+/******************************
+ Main code
+ ******************************/
+
+OUTPUT.innerHTML += ("<p>hi " + " as of " + year + " you are " + age + " years old.</p>");
+OUTPUT.innerHTML += ("<p>you were born in " + birthYear + "</p>");
+OUTPUT.innerHTML += ("<p>in 10 years you will be " + oldAge + "</p>");
+OUTPUT.innerHTML += ("<p>you have " + halfMoney + " pounds</p>");
+OUTPUT.innerHTML += ("<p>you have " + money + " pounds</p>");
+
+
+/******************************
+ Functions
+ ******************************/
+
+ function writeLine() {
+    // Add a line to the html page
+    OUTPUT.innerHTML += "<p>Less code matters</p>";
+}
+function displayWelcome(_name, _age){
+  OUTPUT.innerHTML += "<p>Welcome to the page "+_name+"</p>";
+  OUTPUT.innerHTML += "<p>You are "+_age+" years old</p>";
+}
+displayWelcome("Ms Alice", 16);
+  displayWelcome("Mr Bob", 99);
--- t09_activate_via_button.js
+++ t09_activate_via_button.js
@@ -1,1 +1,18 @@
-/* Paste your code from task 7 here*/
+ function writeLine() {
+    // Add a line to the html page
+    OUTPUT.innerHTML += "<p>Less code matters</p>";
+}
+function displayWelcome(_name, _age){
+  OUTPUT.innerHTML += "<p>Welcome to the page "+_name+"</p>";
+  OUTPUT.innerHTML += "<p>You are "+_age+" years old</p>";
+}
+displayWelcome("Ms Alice", 16);
+  displayWelcome("Mr Bob", 99);
+  
+<button onClick="start()">START</button>
+
+  <p>Open the console <code>(Ctrl-shift-i)</code></p>
+
+<button onclick="start()">Start</button>
+
+<div id="spaceForJavaScriptOutput"></div>
