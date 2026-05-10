9dedc8a11a27f6be59f5ea771dd5502c16cd8dda
24290by <24290by@hvhs.school.nz>
2026-05-10 22:49:46 +0000

    May 10, 2026, 10:49 PM

--- t08_scope.js
+++ t08_scope.js
@@ -1,7 +1,7 @@
 const output = document.getElementById("output");
 
 /* Read the slides, make a prediction, then uncomment this code.
-
+MY PREDICTION: The output will be 0, 1, 2, 0 because the variable in functionTwo is a different variable to the one in functionOne and the global variable.
 
 var myVar = 0;
 output.innerHTML = "Start: "+myVar+"<br>";
