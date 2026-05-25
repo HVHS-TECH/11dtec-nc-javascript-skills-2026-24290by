faa01239d14f04bad9ab7549135fde725b0c5434
24290by <24290by@hvhs.school.nz>
2026-05-25 23:54:15 +0000

    May 25, 2026, 11:54 PM

--- t09_activate_via_button.js
+++ t09_activate_via_button.js
@@ -24,16 +24,11 @@ OUTPUT.innerHTML += ("<p>in 40 years you will be " + oldAge + "</p>");
 OUTPUT.innerHTML += ("<p>you have " + halfMoney + " pounds</p>");
 OUTPUT.innerHTML += ("<p>you have " + money + " pounds</p>");
 
-function writeLine() {
-    // Add a line to the html page
-    OUTPUT.innerHTML += "<p>Less code matters</p>";
+function start() {
+    spaceForJavaScriptOutput.innerHTML
+    spaceForJavaScriptOutput.innerHTML
+    answer = currentYear - userAge;
+    spaceForJavaScriptOutput.innerHTML
 }
-function displayWelcome(_name, _age){
-  OUTPUT.innerHTML += "<p>Welcome to the page "+_name+"</p>";
-  OUTPUT.innerHTML += "<p>You are "+_age+" years old</p>";
-}
-displayWelcome("Jiggle", 16);
-  displayWelcome("Mr BobTheGerbal", 99);
-
 
   
