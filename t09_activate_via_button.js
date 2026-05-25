ba180baa10ee1c8be3388babcfc7f007ea12fc18
24290by <24290by@hvhs.school.nz>
2026-05-25 23:57:36 +0000

    May 25, 2026, 11:57 PM

--- t09_activate_via_button.js
+++ t09_activate_via_button.js
@@ -18,11 +18,8 @@ let OUTPUT = document.getElementById("spaceForJavaScriptOutput")
  Main code
  ******************************/
 
-OUTPUT.innerHTML += ("<p>hi " + " as of " + year + " you are " + age + " years old.</p>");
-OUTPUT.innerHTML += ("<p>you were born in " + birthYear + "</p>");
-OUTPUT.innerHTML += ("<p>in 40 years you will be " + oldAge + "</p>");
-OUTPUT.innerHTML += ("<p>you have " + halfMoney + " pounds</p>");
-OUTPUT.innerHTML += ("<p>you have " + money + " pounds</p>");
+
+
 
 function start() {
     spaceForJavaScriptOutput.innerHTML
@@ -31,5 +28,16 @@ function start() {
     spaceForJavaScriptOutput.innerHTML
 }
 
-
-  
+  function start() {
+    OUTPUT.innerHTML += ("<p>hi " + " as of " + year + " you are " + age + " years old.</p>");
+OUTPUT.innerHTML += ("<p>you were born in " + birthYear + "</p>");
+OUTPUT.innerHTML += ("<p>in 40 years you will be " + oldAge + "</p>");
+OUTPUT.innerHTML += ("<p>you have " + halfMoney + " pounds</p>");
+OUTPUT.innerHTML += ("<p>you have " + money + " pounds</p>");
+}
+function displayWelcome(_name, _age){
+  OUTPUT.innerHTML += "<p>Welcome to the page "+_name+"</p>";
+  OUTPUT.innerHTML += "<p>You are "+_age+" years old</p>";
+}
+displayWelcome("Ms Alice", 16);
+  displayWelcome("Mr Bob", 99);
