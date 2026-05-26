f2574573c526d4b4e5f50cf4196f6d45e6ffaee2
24290by <24290by@hvhs.school.nz>
2026-05-26 08:43:24 +0000

    May 26, 2026, 8:43 AM

--- t10_input_from_HTML.js
+++ t10_input_from_HTML.js
@@ -22,12 +22,6 @@ let OUTPUT = document.getElementById("spaceForJavaScriptOutput")
 
 
 
-    spaceForJavaScriptOutput.innerHTML
-    spaceForJavaScriptOutput.innerHTML
-    answer = currentYear - userAge;
-    spaceForJavaScriptOutput.innerHTML
-}
-
   function start() {
     OUTPUT.innerHTML += ("<p>hi " + " as of " + year + " you are " + age + " years old.</p>");
 OUTPUT.innerHTML += ("<p>you were born in " + birthYear + "</p>");
@@ -38,7 +32,7 @@ OUTPUT.innerHTML += ("<p>you have " + money + " pounds</p>");
 function displayWelcome(_name, _age){
   OUTPUT.innerHTML += "<p>Welcome to the page "+_name+"</p>";
   OUTPUT.innerHTML += "<p>You are "+_age+" years old</p>";
-}
+
 displayWelcome("Ms Alice", 16);
   displayWelcome("Mr Bob", 99);
 
