bacdf8ea46a725de260670b333bfbd80957ba5a7
24290by <24290by@hvhs.school.nz>
2026-05-26 00:00:46 +0000

    May 26, 2026, 12:00 AM

---
+++ t10_input_from_HTML.js
@@ -0,0 +1,51 @@
+/******************************
+ Name of task: Maths
+ ******************************/
+
+
+//Variables 
+let username = "Ben";
+let year = 2026;
+let age = 28;
+let money = 20;
+let birthYear;
+let oldAge;
+ oldAge = age + 10;
+ birthYear - year - age;
+
+let OUTPUT = document.getElementById("spaceForJavaScriptOutput")
+/******************************
+ Main code
+ ******************************/
+
+
+
+
+function start() {
+    spaceForJavaScriptOutput.innerHTML
+    spaceForJavaScriptOutput.innerHTML
+    answer = currentYear - userAge;
+    spaceForJavaScriptOutput.innerHTML
+}
+
+  function start() {
+    OUTPUT.innerHTML += ("<p>hi " + " as of " + year + " you are " + age + " years old.</p>");
+OUTPUT.innerHTML += ("<p>you were born in " + birthYear + "</p>");
+OUTPUT.innerHTML += ("<p>in 40 years you will be " + oldAge + "</p>");
+OUTPUT.innerHTML += ("<p>you have " + halfMoney + " pounds</p>");
+OUTPUT.innerHTML += ("<p>you have " + money + " pounds</p>");
+}
+function displayWelcome(_name, _age){
+  OUTPUT.innerHTML += "<p>Welcome to the page "+_name+"</p>";
+  OUTPUT.innerHTML += "<p>You are "+_age+" years old</p>";
+}
+displayWelcome("Ms Alice", 16);
+  displayWelcome("Mr Bob", 99);
+
+
+
+  <form onsubmit="return false;">
+  <label for="formName">Name:</label>
+  <input type="text" id="nameField" name="formName">
+  <input type="submit" onClick=getFormInput()>
+</form>
