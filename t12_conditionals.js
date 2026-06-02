9ce914f62156dadf558cd7dc7a86ccff4f92ff3e
24290by <24290by@hvhs.school.nz>
2026-06-02 00:17:21 +0000

    Jun 2, 2026, 12:17 AM

--- t12_conditionals.js
+++ t12_conditionals.js
@@ -33,7 +33,20 @@ function getFormInput(){
  Main code
  ******************************/
 
+if (age < 70){
+    console.log("Definitely too young")
+}
 
+if(age >= 70){
+    console.log("Might be too old")
+}
+
+if(age == 70){
+    console.log("Just right")
+}
+
+
+ 
 
 
 console.log("Hello world");
