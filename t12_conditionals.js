bf4523456caf6e9aa5af838910742fe90824c187
24290by <24290by@hvhs.school.nz>
2026-06-03 02:36:16 +0000

    Jun 3, 2026, 2:36 AM

--- t12_conditionals.js
+++ t12_conditionals.js
@@ -1,4 +1,4 @@
-/* Paste your code from the last task *//******************************
+/******************************
  Name of task: Maths
  ******************************/
 
@@ -23,55 +23,25 @@ function welcome(){
 
 
 function displayProduct(_name,_price){
-  OUTPUT.innerHTML += "<p>The product "+_name+" costs "+_price+" pounds</p>";
+  OUTPUT.innerHTML += "<p>The product "+_name+" costs "+_price+" mOney</p>";
 }
 function getFormInput(){
   const NAME_FIELD = document.getElementById("nameField");
   username = NAME_FIELD.value;
-
-/******************************
- Main code
- ******************************/
-
-if (age < 70){
-    console.log("Definitely too young ")
-}
-
-if(age >= 70){
-    console.log("Might be too old")
-}
-
-if(age == 70){
-    console.log("Just right")
-}
-
-
- 
-
-
-console.log("Hello world");
-  OUTPUT.innerHTML = "<p>Your New Balance "+balance+"</p>";
-  OUTPUT.innerHTML += "<p>You are "+age+" years old</p>";
-
-
-welcome();
 }
 
 function showNewAge(){
+  const NAME_FIELD = document.getElementById("nameField");
+  username = NAME_FIELD.value;
+  
   const AGE_FIELD = document.getElementById("ageField");
   const inputAge = Number(AGE_FIELD.value);
   if (isNaN(inputAge)){
-    OUTPUT.innerHTML += "<p>Please enter a valid age</p>";
+    OUTPUT.innerHTML = "<p>put pocket money</p>";
     return;
   }
-
-  if (inputAge > 70) {
-    OUTPUT.innerHTML += "<p>You can afford the chocolate</p>";
-  } else if (inputAge < 70) {
-    OUTPUT.innerHTML += "<p>You cannot afford the chocolate</p>";
-  } else {
-    OUTPUT.innerHTML += "<p>You are exactly 70 — just right</p>";
-  }
+  OUTPUT.innerHTML = "<p>Name "+username+"</p>";
+  OUTPUT.innerHTML += "<p>pocket money: "+inputAge+"</p>";
 }
 
 
